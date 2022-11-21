760c65cc53ffa4acaaf358790b3d3d57320b8fe1
Yogi_Hariyani <hariyaniyogi7292@gmail.com>
2022-11-21 11:22:44 +0530

    add not found router page

---
+++ src/Pages/NotFound/NotFound.tsx
@@ -0,0 +1,5 @@
+function NotFound() {
+    return <div className="text-3xl text-sky-500 p-6 rounded-3xl border border-sky-500">NotFound Component</div>;
+  }
+  export default NotFound;
+  
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
+import NotFound from '../Pages/NotFound/NotFound';
 import PrivateRoutes from './PrivateRoutes/PrivateRoute';
 import PublicRoutes from './PublicRoutes/PublicRoutes';
 
@@ -14,6 +15,8 @@ function MainRoutes() {
       <Route path="/login" element={<PublicRoutes />}>
         <Route path="/login" element={<Login />} />
       </Route>
+
+      <Route path="*" element={<NotFound/>} />
     </Routes>
   );
 }
